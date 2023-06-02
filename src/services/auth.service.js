6133cd129b542fde7a0d4f9b4fd660de2237428a
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-06-02 20:14:25 +0200

    first commit

---
+++ src/services/auth.service.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+export default class AuthService {
+	constructor() {
+		this.API_URL = axios.create({
+			baseURL: process.env.REACT_APP_SERVER_URL,
+		});
+		this.API_URL.interceptors.request.use(
+			(config) => {
+				const storedToken = localStorage.getItem('authToken');
+
+				if (storedToken) {
+					config.headers = { Authorization: `Bearer ${storedToken}` };
+				}
+
+				return config;
+			},
+			(error) => {
+				Promise.reject(error);
+			}
+		);
+	}
+
+	login = (req) => {
+		return this.API_URL.post('/auth/login', req);
+	};
+
+	signup = (req) => {
+		return this.API_URL.post('/auth/signup', req);
+	};
+
+	verify = () => {
+		return this.API_URL.get('/auth/verify');
+	};
+}
