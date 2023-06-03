be8743a28bfb7c80c7a97a4715964f26d617c8ce
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-06-03 23:08:31 +0200

    adaptando el template

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,47 @@
-import "./Navbar.css";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../../context/auth.context";
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/auth.context';
 
 function Navbar() {
-  // Subscribe to the AuthContext to gain access to
-  // the values from AuthContext.Provider's `value` prop
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+	// Subscribe to the AuthContext to gain access to
+	// the values from AuthContext.Provider's `value` prop
+	const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
-  return (
-    <nav>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+	return (
+		<nav>
+			<Link to='/'>
+				<button className='btn btn--link'>Home</button>
+			</Link>
 
-      {isLoggedIn && (
-        <>
-          <button onClick={logOutUser}>Logout</button>
+			{isLoggedIn && (
+				<>
+					<button onClick={logOutUser} className='btn btn--link'>
+						Logout
+					</button>
 
-          <Link to="/profile">
-            <button>Profile</button>
-            {/* <img src="https://picsum.photos/id/402/200/300" style={{ width: 50, height: 50, borderRadius: 25}} alt="profile" /> */}
-          </Link>
+					<Link className='btn--profile' to='/profile'>
+						<button className='btn btn--link'>
+							{user && user.name}
+						</button>
+					</Link>
+				</>
+			)}
 
-          <span>{user && user.name}</span>
-        </>
-      )}
-
-      {!isLoggedIn && (
-        <>
-          <Link to="/signup">
-            {" "}
-            <button>Sign Up</button>{" "}
-          </Link>
-          <Link to="/login">
-            {" "}
-            <button>Login</button>{" "}
-          </Link>
-        </>
-      )}
-    </nav>
-  );
+			{!isLoggedIn && (
+				<div>
+					<Link to='/signup'>
+						{' '}
+						<button className='btn btn--link'>Sign Up</button>{' '}
+					</Link>
+					<Link to='/login'>
+						{' '}
+						<button className='btn btn--link'>Login</button>{' '}
+					</Link>
+				</div>
+			)}
+		</nav>
+	);
 }
 
 export default Navbar;
